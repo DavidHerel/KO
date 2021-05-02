8b98c256b81ba93d341db115e63639ea1b80973a
David Herel <hereldav@fel.cvut.cz>
2021-05-02 21:22:46 +0200

    hw04 30% done

--- hw04/main.py
+++ hw04/main.py
@@ -2,6 +2,8 @@
 import math
 import sys
 from collections import deque
+from itertools import product
+import gurobipy as g
 
 import numpy as np
 
@@ -34,4 +36,36 @@ if  __name__ == '__main__':
         #print(S[frame])
         #print("-----")
         frame+=1
+
+    combinations = product(range(n), range(n))
+    c = {}
+    for i, j in combinations:
+        if i != j:
+            c[(i, j)] = np.absolute(S[i][:, -1, :] -S[j][:, 0, :]).sum()
+    for i in range(n):
+        c[(i, n)] = 0
+        c[(n, i)] = 0
+
+    #now LETS init model
+    model = g.Model()
+    model.Params.lazyConstraints = 1
+
+    combinations_second = product(range(n+1), range(n+1))
+
+    x = model.addVars(n+1  ,n+1, vtype=g.GRB.BINARY)
+
+    model.addConstrs(x[i, j] == 1 for i, j in combinations_second if i != j)
+    model.addConstrs(x[j, i] == 1 for i, j in combinations_second if i != j)
+    model.addConstrs(x[i, i] == 0 for i in range(n+1))
+
+    model.setObjective(sum([x[i,j] * c[i, j] for (i, j) in combinations_second if i != j]), sense=g.GRB.MINIMIZE)
+
+    #TODO
+    model.optimize(callback)
+
+    #TODO
+    #callback
+    #najit cyklus
+    # a je to
+
 
