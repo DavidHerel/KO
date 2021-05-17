f1d73f8c59ef214cbdc95a0cca45c90cc3bd0139
David Herel <hereldav@fel.cvut.cz>
2021-05-17 13:33:45 +0200

    refactoring done

--- hw05/main.py
+++ hw05/main.py
@@ -5,79 +5,81 @@ import sys
 import numpy as np
 from copy import copy
 
-best_cost = float(np.inf)
-best_sol = []
+p,r,d = [], [], []
+best_sol, best_cost = [], np.inf
+glob_was_found = False
 
-def bradley(p, r, d, rest_tasks, c, upper, solution):
-    global best_cost, best_sol
+def bratley(rest_tasks, c, upper, solution, int_time):
+    global p,r,d, best_sol, best_cost, glob_was_found
 
     stop_backtracking = False
+    #3) stop backtracking
     #stopping condition for recursion
     if (rest_tasks == []):
-        return True, stop_backtracking, c, solution
+        return c, solution, True, stop_backtracking, int_time
+    else:
+        was_found = False
+        curr_cost = np.inf
+        sum_p = 0
+        r_j = []
+        for i_k in range(len(rest_tasks)):
+            i = rest_tasks[i_k]
+            r_j.append(r[i])
+            sum_p += p[i]
+        if c <=min(r_j):
+            #do not backtrack
+            stop_backtracking = True
 
-    r_j = []
-    for i in rest_tasks:
-        r_j.append(r[i])
-    if c <=min(r_j):
-        #do not backtrack
-        stop_backtracking = True
+        #2)
+        #Bound on the solution
+        lb = max(c, min(r_j)) + sum_p
+        #prune it
+        if (lb >= upper):
+            print("Pruned on 2. bound on the solution")
+            return c, solution, was_found, stop_backtracking, int_time
 
-    #1)
-    # missed deadline
-    for i in rest_tasks:
-        #prune this node
-        if (max(c, r[i])+p[i] > d[i]):
-            print("Pruned on 1. missed deadline")
-            return False, stop_backtracking, c, solution
+        #1)
+        # missed deadline
+        for i_k in range(len(rest_tasks)):
+            i = rest_tasks[i_k]
+            #prune this node
+            if (max(c, r[i])+p[i] > d[i]):
+                print("Pruned on 1. missed deadline")
+                return c, solution, was_found, stop_backtracking, int_time
 
-    #2)
-    #Bound on the solution
-    sum_p = 0
-    for i in rest_tasks:
-        sum_p += p[i]
-    lb = max(c, min(r_j)) + sum_p
-    #prune it
-    if (lb >= upper):
-        print("Pruned on 2. bound on the solution")
-        return False, stop_backtracking, c, solution
+        for i_k in range(len(rest_tasks)):
+            i = rest_tasks[i_k]
+            temp_task = rest_tasks.copy()
+            temp_task.remove(i)
+            int_time = max(int_time, p[i]) + i
+            cost_upg = max(c, r[i])
+            new_sol = solution.copy()
+            new_sol.append(i)
 
+            temp_cost, temp_sol, found, stop_backtrack, int_time  = bratley(temp_task, cost_upg + p[i], upper, new_sol, int_time)
 
-    was_found = False
-    glob_stop_backtrack = stop_backtracking
-    curr_cost = float(np.inf)
-    curr_sol = []
-    for i in rest_tasks:
-        new_sol = solution + [i]
-        new_cost = max(r[i], c) + p[i]
-
-        # temp_task = copy(rest_tasks)
-        # temp_task.pop(i)
-        temp_task = []
-        for j in rest_tasks:
-            if j !=i:
-                temp_task.append(j)
-
-        found, stop_backtrack, temp_cost, temp_sol = bradley(p, r, d, temp_task, new_cost, upper, new_sol)
-
-        if (found):
-            was_found = True
-            # if temp_cost < best_cost:
-            #     print("Best sol found")
-            #     best_sol = temp_sol
-            #     best_cost = temp_cost
-            #     upper = temp_cost
-            if temp_cost < curr_cost:
-                curr_sol = temp_sol
-                curr_cost = temp_cost
-                upper = temp_cost
-                print("Best sol found " + str(curr_sol) + "  " + str(curr_cost))
-        glob_stop_backtrack = glob_stop_backtrack or stop_backtracking
-        if stop_backtrack:
-            print("Break pri i: " + str(i))
-            break
-
-    return was_found, glob_stop_backtrack, curr_cost, curr_sol
+            if (int_time >= 0 and found):
+                if temp_cost < best_cost:
+                    #print("Best sol found")
+                    best_sol = temp_sol
+                    int_time+=1
+                    best_cost = temp_cost
+                    upper = temp_cost
+                    #print("Tady")
+                if temp_cost < curr_cost:
+                    int_time += 1
+                    curr_cost = temp_cost
+                    upper = temp_cost
+                    #print("Tady")
+                    #print("Best sol found " + str(curr_sol) + "  " + str(curr_cost))
+                was_found = True
+                glob_was_found = True
+            if int_time >= 0 and stop_backtrack:
+                stop_backtracking = True
+                print("Break pri i: " + str(i))
+                print("Int time "  + str(int_time))
+                return best_cost, best_sol, was_found, stop_backtracking, int_time
+        return best_cost, best_sol, was_found, stop_backtracking, int_time + 1
 
 if  __name__ == '__main__':
     #take 2 arguments (input and output)
@@ -94,6 +96,8 @@ if  __name__ == '__main__':
     r=[]
     d=[]
     rest_tasks = []
+    final_starting_times = ["-1" for i in range(n)]
+    offset = 0
     for i in range(n):
         first_line = list(map(int, f.readline().split()))
         p.append(first_line[0])
@@ -108,22 +112,20 @@ if  __name__ == '__main__':
     f.close()
     ub = max(d) + 1
     print(ub)
-
-    solution, _, _, cur_sol = bradley(p, r, d, rest_tasks, 0, ub, [])
-    print(cur_sol)
-    if solution:
+    int_time = 0
+    bratley(rest_tasks, 0, ub, [], int_time)
+    print(best_sol)
+    if glob_was_found:
         print("solution found")
-        pp = [0 for _ in range(n)]
         f = open(output, "w")
-        start = 0
-        answ = []
-        for i in cur_sol:
-            pp[i] = max(start, r[i])
-            #f.write(str(max(start, r[i])) + "\n")
-            print(max(start, r[i]))
-            #answ.append(max(start, r[i]))
-            start = max(start, r[i]) + p[i]
-        f.write('\n'.join(map(str, pp)))
+        for i_k in range(len(best_sol)):
+            i = best_sol[i_k]
+            final_starting_times[i] = str(max(r[i], offset))
+            #print("Offset: " + str(start) + " " + " r_i: " + str(r[i]))
+            #print(max(r[i], offset))
+            offset = int(final_starting_times[i]) + p[i]
+        for i_k in range(len(final_starting_times)):
+            f.write(final_starting_times[i_k] + "\n")
         f.close()
     else:
         print("Solution not found")
